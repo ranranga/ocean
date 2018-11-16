682c337501334cc75a4c58cf3a526aae325699d6
thescad <sachidanand.vaishnav@gmail.com>
2018-11-16 10:56:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ocean
+
+An E-Commerce Portal
---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    database: 'mongodb://scadyy:abc123@ds039301.mlab.com:39301/oceanwebapplication',
+    port: 3030,
+    secret: 'SachidanandVaishnav15112018'
+}
