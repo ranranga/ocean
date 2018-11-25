465791dabe8a6fc0906bddfd129ad8eb3e18f24f
scadyy <sachidanand.vaishnav@gmail.com>
2018-11-25 12:32:39 +0530

    Added address api and route

--- server/routes/account.js
+++ server/routes/account.js
@@ -102,4 +102,34 @@ router.route('/profile')
     });
 });
 
+router.route('/address')
+.get(checkJWT, (req, res, next) => {
+    User.findOne({ _id: req.decoded.user._id }, (err, user) => {
+        res.json({
+            success: true,
+            address: user.address,
+            message: "Successful"
+        });
+    });
+})
+.post(checkJWT, (req, res, next) => {
+    User.findOne({ _id: req.decoded.user._id }, (err, user) => {
+        if (err) return next(err); 
+
+        if (req.body.addr1) user.address.addr1 = req.body.addr1;
+        if (req.body.addr2) user.address.addr2 = req.body.addr2;
+        if (req.body.city) user.address.city = req.body.city;
+        if (req.body.state) user.address.state = req.body.state;
+        if (req.body.country) user.address.country = req.body.country;
+        if (req.body.postalCode) user.address.postalCode = req.body.postalCode;
+
+        user.save();
+        console.log(JSON.stringify(user));
+        res.json({
+            success: true,
+            message: 'Successfully edited your address'
+        });
+    });
+});
+
 module.exports = router;
